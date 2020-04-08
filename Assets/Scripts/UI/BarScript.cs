92b797ce9253e1e4004c9e0975df804ac00edf75
Erik Högberg <erik.hogberg.9945@student.uu.se>
2020-04-08 17:54:57 +0200

    added boost limit, boost bar in UI, separate velocity cap for boosting

---
+++ Assets/Scripts/UI/BarScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[RequireComponent(typeof(Image))]
+public class BarScript : MonoBehaviour {
+
+	// private Image barFill;
+
+	void Start() {
+		// barFill = GetComponent<Image>();
+	}
+
+	public void SetBarPercentage(float percentage) {
+		if (percentage > 1)
+			percentage = 1;
+		
+		if (percentage < 0)
+			percentage = 0;
+		
+		
+		Vector3 scale = transform.localScale;
+		scale.x = percentage;
+		transform.localScale = scale;
+	}
+
+}
