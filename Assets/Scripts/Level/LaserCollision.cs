3af09fe5d7e303f110646c303f72db8f819f1a53
Ella Fernström <ella.fernstrom@gmail.com>
2020-04-15 17:42:46 +0200

    Laser wheel prefab

---
+++ Assets/Scripts/Level/LaserCollision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserCollision : MonoBehaviour
+{
+	private LaserWheelControls wheel;
+
+	void Start() {
+		wheel = transform.parent.GetComponent<LaserWheelControls>();
+
+    }
+
+	private void OnTriggerEnter(Collider other)
+    {
+		wheel.LogHit();
+		
+	}
+}
---
+++ Assets/Scripts/Level/LaserWheelControls.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserWheelControls : MonoBehaviour
+{
+	private GameObject Laser1;
+	private GameObject Laser2;
+	private GameObject Laser3;
+	private GameObject Laser4;
+
+	private bool active1 = true;
+
+	void Start()
+	{
+		Laser1 = transform.Find("Laser1").gameObject;
+		Laser2 = transform.Find("Laser2").gameObject;
+		Laser3 = transform.Find("Laser3").gameObject;
+		Laser4 = transform.Find("Laser4").gameObject;
+
+	}
+
+	public void LogHit()
+    {
+		Debug.Log("Laser Hit!");
+    }
+}
