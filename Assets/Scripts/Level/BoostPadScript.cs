038acbfa21d8c5eb68523eca726de1fd7f94a43e
Erik Högberg <erik.hogberg.9945@student.uu.se>
2020-04-20 18:32:12 +0200

    basic boost pad

---
+++ Assets/Scripts/Level/BoostPadScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoostPadScript : MonoBehaviour {
+
+	[Tooltip("Speed to either set or add")]
+	public float Speed = 1f;
+	[Tooltip("If the speed should be added instead of set")]
+	public bool AddSpeed = false;
+	[Tooltip("If the car should be rotated to face the direction of the boost pad")]
+	public bool SetDirection = false;
+
+	private void OnTriggerEnter(Collider other) {
+		var rb = other.attachedRigidbody; //.GetComponent<Rigidbody>();
+
+		if (!rb)
+			return;
+
+		if (SetDirection){
+			rb.MoveRotation(transform.rotation);
+			rb.velocity = transform.forward * rb.velocity.magnitude;
+		}
+
+		if (AddSpeed)
+			rb.velocity += Vector3.Normalize(rb.velocity) * Speed;
+		else if (rb.velocity.sqrMagnitude < Speed * Speed)
+			rb.velocity = Vector3.Normalize(rb.velocity) * Speed;
+
+	}
+
+}
