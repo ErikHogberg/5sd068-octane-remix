aedde00f2b0a6d4a742ec0c770fbee4d1d8642f7
Erik Högberg <erik.hogberg.9945@student.uu.se>
2020-04-21 17:43:35 +0200

    Attempt at generating UI buttons from level segments will probably scrap it and use 3d mouse picking instead

---
+++ Assets/Scripts/Level/Environment/LevelPieceSuperClass.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class LevelPieceSuperClass : MonoBehaviour {
+
+	public static List<LevelPieceSuperClass> Pieces = new List<LevelPieceSuperClass>();
+
+	// TODO: register pieces globally
+	// TODO: manipulate registered pieces in UI
+
+	// TODO: comfortable way to reference obstacles avalable for placement
+
+	private void Awake() {
+		Pieces.Add(this);
+	}
+
+
+}
---
+++ Assets/Scripts/Level/RemixMap/RemixMapScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RemixMapScript : MonoBehaviour {
+
+	public Camera MapCamera;
+
+	public GameObject ButtonPrefab;
+	public RawImage MapImage;
+
+	void Start() {
+
+		foreach (var piece in LevelPieceSuperClass.Pieces) {
+
+			Button button = Instantiate(
+				ButtonPrefab,
+				// MapCamera.transform.position - piece.transform.position,
+				// Quaternion.identity,
+				MapImage.transform
+			).GetComponent<Button>();
+			
+			button.transform.localPosition = (piece.transform.position - MapCamera.transform.position) * MapImage.rectTransform.localScale.x;
+			
+
+			// TODO: 2D pos, check if z is correct
+
+
+
+		}
+
+	}
+
+}
