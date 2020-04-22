92a57d40265130f08d5f1191e7daaf59718e908e
Erik Högberg <erik.hogberg.9945@student.uu.se>
2020-04-22 18:09:05 +0200

    script for updating mesh colliders with altered meshes

---
+++ Assets/Scripts/System/UpdateMeshColliderScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateMeshColliderScript : MonoBehaviour {
+	SkinnedMeshRenderer meshRenderer;
+	MeshCollider meshCollider;
+
+	private void Awake() {
+		meshRenderer = GetComponent<SkinnedMeshRenderer>();
+		meshCollider = GetComponent<MeshCollider>();
+		UpdateMeshCollider();
+	}
+
+	public void UpdateMeshCollider() {
+		// Source: https://answers.unity.com/questions/39490/collider-on-skinned-mesh.html
+		Mesh colliderMesh = new Mesh();
+		meshRenderer.BakeMesh(colliderMesh);
+		meshCollider.sharedMesh = null;
+		meshCollider.sharedMesh = colliderMesh;
+	}
+}
