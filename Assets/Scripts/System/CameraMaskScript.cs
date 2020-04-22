be003242b444f70dee9345212b72516804c4e9b9
Erik Högberg <erik.hogberg.9945@student.uu.se>
2020-04-22 13:42:52 +0200

    singleton components for setting camera mask and DontDestroyOnLoad

---
+++ Assets/Scripts/System/CameraMaskScript.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraMaskScript : MonoBehaviour {
+
+	public static CameraMaskScript MainInstance;
+	// IDEA: camera list for splitscreen, etc.
+
+	[Serializable]
+	public struct CameraMaskTableRow {
+		public string ID;
+		public LayerMask CameraMask;
+	}
+
+	public List<CameraMaskTableRow> CameraMaskTable;
+
+    private LayerMask defaultMask;
+
+    private Camera attachedCamera;
+
+    private void Awake() {
+        attachedCamera = GetComponent<Camera>();
+        defaultMask = attachedCamera.cullingMask;
+        // IDEA: set event mask too?
+    }
+
+	public bool SetMask(string key) {
+		foreach (var maskRow in CameraMaskTable) {
+			if (maskRow.ID == key) {
+                
+                return true;
+			}
+		}
+
+		return false;
+	}
+
+
+}
