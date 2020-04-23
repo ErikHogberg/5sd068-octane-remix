99a484092b977da65e02150bd9032cbca6b321f9
Erik Högberg <erik.hogberg.9945@student.uu.se>
2020-04-23 23:40:12 +0200

    some remix system progress, added generic object un-hider for obstacles

---
+++ Assets/Scripts/System/ObjectSelectorScript.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Hides and unhides objects in list to only show one at a time, decided on string key supplied
+public class ObjectSelectorScript : MonoBehaviour {
+
+	[Serializable]
+	public struct ObjectIndex {
+		public string Key;
+		public GameObject Value;
+	}
+
+	public List<ObjectIndex> objects;
+
+	public GameObject ShownObject { get; private set; }
+
+	public void UnhideObject(string key) {
+		if (ShownObject)
+			ShownObject.SetActive(false);
+
+		if (key == "") 
+			return;
+
+		foreach (var gameObject in objects) {
+			if (gameObject.Key == key) {
+				ShownObject = gameObject.Value;
+				ShownObject.SetActive(true);
+				return;
+			}
+		}
+	}
+
+}
