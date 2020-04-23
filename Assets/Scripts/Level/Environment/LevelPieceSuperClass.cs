53c2b9d70af361927acf00b4109aca59f42c80a3
Erik Högberg <erik.hogberg.9945@student.uu.se>
2020-04-23 17:56:14 +0200

    akash car working

--- Assets/Scripts/Level/Environment/LevelPieceSuperClass.cs
+++ Assets/Scripts/Level/Environment/LevelPieceSuperClass.cs
@@ -17,6 +17,10 @@ public abstract class LevelPieceSuperClass : MonoBehaviour {
 
 	// IDEA: option to disallow placing obstacles on segment
 
+	// IDEA: ability select multiple segments, shift click? show blank/custom message if same setting is different for some objects selected
+	// IDEA: ability to group segments together, selecting and altering all segments at the same time
+	// IDEA: when selecting multiple: list all avaliable settings, set them for only the segments that the settings can be applied for
+
 
 	private void Awake() {
 		Pieces.Add(this);
@@ -31,7 +35,6 @@ public abstract class LevelPieceSuperClass : MonoBehaviour {
 		} else if (Input.GetMouseButtonDown(1)) {
 			RemixMapScript.StartRotate();
 		}
-
 
 	}
 
