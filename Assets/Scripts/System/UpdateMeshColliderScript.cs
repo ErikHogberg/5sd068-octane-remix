f1e9f90cb2110072b9611dec873d564126482829
Erik Högberg <erik.hogberg.9945@student.uu.se>
2020-04-23 13:12:45 +0200

    minor changes from yesterday

--- Assets/Scripts/System/UpdateMeshColliderScript.cs
+++ Assets/Scripts/System/UpdateMeshColliderScript.cs
@@ -16,7 +16,11 @@ public class UpdateMeshColliderScript : MonoBehaviour {
 		// Source: https://answers.unity.com/questions/39490/collider-on-skinned-mesh.html
 		Mesh colliderMesh = new Mesh();
 		meshRenderer.BakeMesh(colliderMesh);
+		// meshCollider.sharedMesh.Clear(keepVertexLayout: true); // NOTE: crashes unity
+		// var oldMesh = meshCollider.sharedMesh;
+		// TODO: destroy or reuse old mesh
 		meshCollider.sharedMesh = null;
+		// Destroy(oldMesh);
 		meshCollider.sharedMesh = colliderMesh;
 	}
 }
