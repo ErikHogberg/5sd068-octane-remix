b671f7bba4344d54a238bb934044b3402d9da94e
Erik Högberg <erik.hogberg.9945@student.uu.se>
2020-04-28 14:10:59 +0200

    more option for order of track segments

--- Assets/Scripts/Level/Environment/LevelPieceSuperClass.cs
+++ Assets/Scripts/Level/Environment/LevelPieceSuperClass.cs
@@ -22,19 +22,27 @@ public abstract class LevelPieceSuperClass : MonoBehaviour {
 	public int SegmentOrder;
 	// [Tooltip("If resetting due to out of order segment should be ignored")]
 	// public bool OverrideSegmentOrderReset = false;
-	public bool OverridePreviousSegment = false;
-	public int PreviousSegment = 0;
-	
 
-	// IDEA: define an override "previous segment" instead of disabling all skip reset checks
-	// IDEA: apply allowed skip number to previous segment overrides too
+	[Tooltip("Override which segment was before this one, instead of assuming segment order - 1")]
+	public bool OverridePreviousSegment = false;
+	[Tooltip("Which segments were before this one, requires the override to be checked to be used")]
+	// public int PreviousSegment = 0;
+	public List<int> PreviousSegments;
+
+	[Tooltip("Override how many segments are allowed to be skipped when entering this segment")]
+	public bool OverrideSegmentSkip = false;
+	[Tooltip("How many segments are allowed to be skipped when entering this segment, 0 means only the exact previous segment is allowed")]
+	[Min(0)]
+	public int CustomSegmentSkip = 0;
+	// NOTE: will not check overrides of previous segments other than for this segment
+	// IDEA: define list of multiple allowed previous segments?
 
 	public Transform RespawnSpot;
 
 	// IDEA: empty level segment type for optional spots for adding roads
 	// IDEA: dynamic list of segment editing fields, only show the settings allowed for specific class, pushing fields from script every update
 
-	// IDEA: option to disallow placing any obstacles on segment
+	// IDEA: option to disallow placing any obstacles on segment, just dont add any obstacles to object selector?
 
 	// IDEA: ability select multiple segments, shift click? show blank/custom message if same setting is different for some objects selected
 	// IDEA: ability to group segments together, selecting and altering all segments at the same time
@@ -69,11 +77,23 @@ public abstract class LevelPieceSuperClass : MonoBehaviour {
 	// TODO: fade effect when respawning
 
 	private void OnTriggerEnter(Collider other) {
+
+		int currentSegmentSkip = allowedSegmentSkip;
+
+		if (OverrideSegmentSkip) 
+			currentSegmentSkip = CustomSegmentSkip;
+
 		if (
-			// OverrideSegmentOrderReset || // if skip mitigation resetting is disabled on this segment
-			SegmentOrder <= currentSegmentIndex + 1 + allowedSegmentSkip // if on next correct segment in allowed range
-			&& SegmentOrder >= currentSegmentIndex - 1 - allowedSegmentSkip // if on previous correct segment in allowed range
-			|| (currentSegmentIndex == Segments.Count - 1 && SegmentOrder == 0) // loop track
+			// NOTE: possible false positive when using override?
+			(
+				OverridePreviousSegment
+				&& PreviousSegments.Contains(currentSegmentIndex)
+			)
+			|| (
+				SegmentOrder <= currentSegmentIndex + 1 + currentSegmentSkip // if on next correct segment in allowed range
+				&& SegmentOrder >= currentSegmentIndex - 1 - currentSegmentSkip
+			) // if on previous correct segment in allowed range
+			// || (currentSegmentIndex == Segments.Count - 1 && SegmentOrder == 0) // loop track
 		) {
 			currentSegmentIndex = SegmentOrder;
 			print("current segment: " + currentSegmentIndex);
