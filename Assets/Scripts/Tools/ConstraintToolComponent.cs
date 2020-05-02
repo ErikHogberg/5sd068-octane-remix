e0d72858681c2d3b5cafc255755ee5aafe8164aa
Erik Högberg <erik.hogberg.9945@student.uu.se>
2020-05-02 02:41:33 +0200

    attempt at constraints when moving bones using custom editor tool

---
+++ Assets/Scripts/Tools/ConstraintToolComponent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class ConstraintToolComponent : MonoBehaviour {
+
+	[Tooltip("Min and max allowed distance of all constrained transforms")]
+	public Vector2 DistanceConstraint; 
+	// IDEA: float value for deadzone compared to init distance instead of min-max vector
+
+	public bool ConstrainAngle;
+	public float AngleConstraint;
+
+
+
+	public List<Transform> ConstrainedTransforms;
+
+	public void EnforceConstraints() {
+		foreach (var item in ConstrainedTransforms) {
+			// TODO: move constrained transforms
+
+			var constraintComponent = item.GetComponent<ConstraintToolComponent>();
+			if (constraintComponent) {
+				constraintComponent.EnforceConstraints();
+			}
+		}
+	}
+
+}
