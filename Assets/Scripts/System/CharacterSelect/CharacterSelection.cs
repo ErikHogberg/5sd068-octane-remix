2079e8c3721cc0dedaf27b8468294e419ae30ed0
Ella Fernström <ella.fernstrom@gmail.com>
2020-05-04 14:41:15 +0200

    Character select WIP

---
+++ Assets/Scripts/System/CharacterSelect/CharacterSelection.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public enum CharacterSelected
+{
+	NONE = 0,
+	AKASH,
+	MICHISHIGE,
+	LUDWIG
+}
+
+[System.Serializable]
+public class CharSelectEntry
+{
+	[Tooltip("The name that will show up in the UI for this car.")]
+	public string carName;
+
+	[Tooltip("The selection tag associated with this car.")]
+	public CharacterSelected carTag;
+
+	[Tooltip("The game object in the character select environment which contain the purely visual car model.")]
+	public GameObject carModel;
+}
+
+public class CharacterSelection : MonoBehaviour
+{
+	private static CharacterSelection _i;
+	public static CharacterSelection i {
+		get {
+			if (_i == null) { _i = Instantiate(Resources.Load<CharacterSelection>("CharacterSelectEnvironment")); }
+			return null;
+		}
+	}
+
+	//Player index + their character choice
+	private static Dictionary<int, CharacterSelected> choices;
+
+	[Header("Data")]
+	[Tooltip("The data for all character select entries.")]
+	public CharSelectEntry[] charSelectData;
+
+	//Index for easily storing what car we're currently viewing
+	private int currViewIndex = 0;
+
+	//These should probably be found with some other method in the future
+	[Tooltip("To make sure the actual player objects don't do anything while in character selection, put their references here.")]
+	public GameObject[] playerObjects;
+
+
+	private Canvas mainCanvas;
+	private Camera charSelectCamera;
+	private GameObject charSelectVisuals;
+
+	private Dictionary<Camera, bool> cameraStatusMemory;
+
+	void Awake()
+	{
+		_i = this;
+		mainCanvas = CanvasFinder.thisCanvas;
+		cameraStatusMemory = new Dictionary<Camera, bool>();
+		choices = new Dictionary<int, CharacterSelected>();
+		UINavInput.i.AddUINavListener(UIMode.CHARSELECT);
+
+		if (transform.childCount > 2)
+		{
+			Debug.Log("CharacterSelection: Root object has more than its intended 2 children");
+			return;
+		}
+		else
+		{
+			foreach (Transform child in transform)
+			{
+				if (child.GetComponent<Camera>() != null)
+					charSelectCamera = child.gameObject.GetComponent<Camera>();
+				else charSelectVisuals = child.gameObject;
+			}
+			charSelectCamera.enabled = false;
+			charSelectVisuals.SetActive(false);
+		}
+	}
+
+	void Start()
+	{
+		MakePick(0, CharacterSelected.AKASH);
+		ActivateCharSelect(true);
+		//SwapDisplayCar(true);
+	}
+
+	//TODO: Make it button-activate
+	//Sets the current choice of character for a specified player index
+	public void MakePick(int playerIndex, CharacterSelected pick)
+	{
+		if (choices.ContainsKey(playerIndex)) {
+			choices[playerIndex] = pick;
+		}
+		else {
+			choices.Add(playerIndex, pick);
+		}
+	}
+
+	public CharacterSelected CurrentIndex() { return charSelectData[currViewIndex].carTag; }
+
+	public void Test() { bool hi = true; }
+
+	//Goes either left or right through the array of available cars
+	public void SwapDisplayCar(bool goLeft)
+	{
+		charSelectData[currViewIndex].carModel.SetActive(false);
+
+		if (goLeft) currViewIndex -= 1;
+		else currViewIndex += 1;
+
+		if (currViewIndex < 0) currViewIndex = charSelectData.Length - 1;
+		else if (currViewIndex >= charSelectData.Length) currViewIndex = 0;
+
+		VisualSetDisplay();
+	}
+
+	//Swaps immediately to the available car at the specified index
+	public void SetDisplayCar(int index)
+	{
+		charSelectData[currViewIndex].carModel.SetActive(false);
+		currViewIndex = index;
+		VisualSetDisplay();
+	}
+
+	private void VisualSetDisplay()
+	{
+		if (charSelectData[currViewIndex].carTag == choices[0])
+			CharSelectCanvas.i.SetCheck(true);
+		else CharSelectCanvas.i.SetCheck(false);
+
+		CharSelectCanvas.i.SetText(charSelectData[currViewIndex].carName);
+		charSelectData[currViewIndex].carModel.SetActive(true);
+	}
+
+	public void ActivateCharSelect(bool toggle)
+	{
+		foreach (GameObject obj in playerObjects) { obj.SetActive(!toggle); }
+		CanvasFinder.thisCanvas.gameObject.SetActive(!toggle);
+		UINavInput.i.SetUINavMode(UIMode.CHARSELECT);
+
+		if (toggle == true) {
+			Camera[] cameraList = Camera.allCameras;
+			foreach (Camera cam in cameraList) {
+				cameraStatusMemory.Add(cam, cam.enabled);
+				cam.enabled = !toggle;
+			}
+			UINavInput.i.Activate();
+		} else {
+			foreach (KeyValuePair<Camera, bool> cam in cameraStatusMemory) {
+				cam.Key.enabled = cam.Value;
+			}
+			cameraStatusMemory.Clear();
+			UINavInput.i.Deactivate();
+		}
+
+		charSelectVisuals.SetActive(toggle);
+		charSelectCamera.enabled = toggle;
+		CharSelectCanvas.i.Activate(toggle);
+
+		for (int i = 0; i < charSelectData.Length; i++)
+		{
+			//Sorts choices dictionary by key and picks out the item with lowest-value key
+			if (charSelectData[i].carTag == choices.OrderBy(j => j.Key).First().Value)
+			{
+				CharSelectCanvas.i.SetText(charSelectData[i].carName);
+				currViewIndex = i;
+			}
+		}
+	}
+
+
+}
---
+++ Assets/Scripts/UI/UINavInput.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.InputAction;
+
+public enum UIMode
+{
+	HOME = 0,
+	REMIX,
+	CHARSELECT
+}
+
+public class UINavListener
+{
+	protected UIMode mode;
+	protected bool swipeReady = true;
+
+	protected float lastSwipeValue = 0f;
+	protected float lastConfirmValue = 0f;
+	protected float lastCancelValue = 0f;
+
+	public void SwipePing(float swipe) { lastSwipeValue = swipe; ModeSwipe(); }
+	public void ConfirmPing(float confirm) { lastConfirmValue = confirm; ModeConfirm(); }
+	public void CancelPing(float cancel) { lastCancelValue = cancel; ModeCancel(); }
+
+    public UINavListener(UIMode p_mode) { mode = p_mode; }
+
+	protected void ModeSwipe() {
+		if (mode == UIMode.HOME) { }
+		else if (mode == UIMode.REMIX) { }
+		else if (mode == UIMode.CHARSELECT) 
+		{
+			Debug.Log("Swipe: " + lastSwipeValue);
+			if (swipeReady) {
+				if (lastSwipeValue <= -0.5f) {
+					//CharacterSelection.i.SwapDisplayCar(true);
+					swipeReady = false;
+				} else if (lastSwipeValue >= 0.5f) {
+					//CharacterSelection.i.SwapDisplayCar(false);
+				}
+			}
+			else { if (lastSwipeValue == 0f) swipeReady = true; }
+		}
+    }
+	protected void ModeConfirm() {
+		if (mode == UIMode.HOME) { }
+		else if (mode == UIMode.REMIX) { }
+		else if (mode == UIMode.CHARSELECT) {
+			Debug.Log("Confirm: " + lastConfirmValue);
+			if (lastConfirmValue >= 0.5f) CharacterSelection.i.Test();
+				//CharacterSelection.i.MakePick(0, CharacterSelection.i.CurrentIndex());
+		}
+	}
+	protected void ModeCancel() {
+		if (mode == UIMode.HOME) { }
+		else if (mode == UIMode.REMIX) { }
+		else if (mode == UIMode.CHARSELECT) {
+			Debug.Log("Cancel: " + lastCancelValue);
+			if (lastCancelValue >= 0.5f) CharacterSelection.i.ActivateCharSelect(false);
+		}
+	}
+
+	public void ChangeUIMode(UIMode p_mode) { mode = p_mode; }
+}
+
+public class UINavInput : MonoBehaviour
+{
+	private static UINavInput _i;
+	public static UINavInput i {
+		get {
+			if (_i == null) { _i = Instantiate(Resources.Load<UINavInput>("UINav")); }
+			return _i;
+		}
+	}
+
+	[Header("Key bindings")]
+	public InputActionReference sideSwipeKeyBind;
+	public InputActionReference confirmKeyBind;
+	public InputActionReference cancelKeyBind;
+
+	protected float swipeBuffer = 0f;
+	protected float confirmBuffer = 0f;
+	protected float cancelBuffer = 0f;
+
+	private UIMode currentMode;
+	private Dictionary<UIMode, UINavListener> listeners;
+
+	private void SetSwipe(CallbackContext c) {
+		float input = c.ReadValue<float>();
+		swipeBuffer = input;
+		foreach (KeyValuePair<UIMode, UINavListener> listener in listeners) {
+			listener.Value.SwipePing(swipeBuffer);
+        }
+	}
+
+	private void SetConfirm(CallbackContext c) {
+		float input = c.ReadValue<float>();
+		confirmBuffer = input;
+		foreach (KeyValuePair<UIMode, UINavListener> listener in listeners) {
+			listener.Value.ConfirmPing(confirmBuffer);
+		}
+	}
+
+	private void SetCancel(CallbackContext c) {
+		float input = c.ReadValue<float>();
+		cancelBuffer = input;
+		foreach (KeyValuePair<UIMode, UINavListener> listener in listeners) {
+			listener.Value.CancelPing(cancelBuffer);
+		}
+	}
+
+	void Awake() {
+		sideSwipeKeyBind.action.performed += SetSwipe;
+		confirmKeyBind.action.performed += SetConfirm;
+		cancelKeyBind.action.performed += SetCancel;
+
+		currentMode = UIMode.HOME;
+		listeners = new Dictionary<UIMode, UINavListener>();
+		Deactivate();
+	}
+
+	public void Activate() {
+		sideSwipeKeyBind.action.Enable();
+		confirmKeyBind.action.Enable();
+		cancelKeyBind.action.Enable();
+	}
+
+	public void Deactivate() {
+		sideSwipeKeyBind.action.Disable();
+		confirmKeyBind.action.Disable();
+		cancelKeyBind.action.Disable();
+	}
+
+	public void SetUINavMode(UIMode p_mode) { currentMode = p_mode; }
+	public void AddUINavListener(UIMode p_mode) {
+		if (listeners.ContainsKey(p_mode)) {
+			Debug.Log("UINavInput: Listeners list already contains an entry for this UI mode");
+		}
+		else {
+			listeners.Add(p_mode, new UINavListener(p_mode));
+		}
+	}
+}
