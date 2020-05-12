f1435551f571a2d37a1fb91a9ed4b04a8f6b9cd0
Erik Högberg <erik.hogberg.9945@student.uu.se>
2020-05-13 01:32:13 +0200

    added a fun little trampoline

--- Assets/Scripts/Level/BoostPadScript.cs
+++ Assets/Scripts/Level/BoostPadScript.cs
@@ -13,6 +13,10 @@ public class BoostPadScript : MonoBehaviour {
 	[Tooltip("If the speed is added in the direction of the boost pad instead of the car")]
 	public bool SetSpeedInDirection = false;
 
+	[Space]
+	[Tooltip("Assign an object here to use its forward direction instead of the forward direction of this object. The object position can be anywhere")]
+	public Transform OptionalDirectionOverride;
+
 	// TODO: option to either ignore or allow setting or adding speed values that would result in a lower speed
 	// IDEA: if not allowed, interpret value as inverting direction
 	// public bool AllowRemovingSpeed = false;
@@ -23,8 +27,12 @@ public class BoostPadScript : MonoBehaviour {
 		if (!rb)
 			return;
 
+		Transform t =  transform;
+		if (OptionalDirectionOverride)
+			t = OptionalDirectionOverride;
+
 		if (SetDirection) {
-			rb.MoveRotation(transform.rotation);
+			rb.MoveRotation(t.rotation);
 			// rb.velocity = transform.forward * rb.velocity.magnitude;
 		}
 
@@ -34,9 +42,7 @@ public class BoostPadScript : MonoBehaviour {
 		if (SetSpeedInDirection) {
 			if (!AddSpeed)
 				rb.velocity = Vector3.zero;
-			rb.AddForce(transform.forward * Speed, ForceMode.VelocityChange);
-
-
+			rb.AddForce(t.forward * Speed, ForceMode.VelocityChange);
 		} else {
 			if (AddSpeed)
 				rb.velocity += Vector3.Normalize(rb.velocity) * Speed;
