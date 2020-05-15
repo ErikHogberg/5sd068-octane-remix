1efe12cb2c998a260abae24dba995f3b3074b0c6
Erik Högberg <erik.hogberg.9945@student.uu.se>
2020-05-15 15:57:56 +0200

    added boost pad placeholder texture. better car settings. more level design

--- Assets/Scripts/System/CameraSelectorScript.cs
+++ Assets/Scripts/System/CameraSelectorScript.cs
@@ -13,12 +13,14 @@ public class CameraSelectorScript : MonoBehaviour {
 
 	void Update() {
 
-		if (Input.GetKeyDown(KeyCode.Alpha1)) 
+		if (Input.GetKeyDown(KeyCode.Alpha1))
 			selector.UnhideObject(0);
-		else if (Input.GetKeyDown(KeyCode.Alpha2)) 
+		else if (Input.GetKeyDown(KeyCode.Alpha2))
 			selector.UnhideObject(1);
-		else if (Input.GetKeyDown(KeyCode.Alpha3)) 
+		else if (Input.GetKeyDown(KeyCode.Alpha3))
 			selector.UnhideObject(2);
-		
+		else if (Input.GetKeyDown(KeyCode.Alpha4))
+			selector.UnhideObject(3);
+
 	}
 }
