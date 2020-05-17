ba97d4cd34dc0e8d473650e46c1708dca0d85a92
Erik Högberg <erik.hogberg.9945@student.uu.se>
2020-05-17 23:43:45 +0200

    New camera working. Added bumper. Enabled interpolation for all car rbs

--- Assets/Scripts/Car/CarCam2.cs
+++ Assets/Scripts/Car/CarCam2.cs
@@ -2,60 +2,91 @@
 
 public class CarCam2 : MonoBehaviour {
 
-	[Tooltip("Max allowed yaw difference between camera car direction, in degrees")]
-	public float MaxYaw = 45f;
+	// [Tooltip("Max allowed yaw difference between camera car direction, in degrees")]
+	// public float MaxYaw = 45f;
 
 	// public float TurnSpeed = 1f;
-	public float FollowSpeed = 1f;
+	[Tooltip("How fast the camera moves towards straight behind the car")]
+	public float FollowSpeed = 10f;
 	// TODO: velocity curve for turn/follow speed increase
 
-	public Vector3 Buffer;
+	[Space]
 
-	public SteeringScript car;
-	public Transform CameraFocus;
+	[Tooltip("How many degrees the camera is rotates when steering is at max angle")]
+	public float SteeringMax = 20f;
+	[Tooltip("How fast the camera follow the steering angle when the steering angle is at max angle")]
+	public float SteeringFollowSpeed = 2f;
+	[Tooltip("How much of the steering follow speed is used when the steering is 0")]
+	[Range(0, 1)]
+	public float MinSteeringFollowMultiplier = 0.2f;
+	[Tooltip("If the steering rotation is around car up axis instead of world up axis")]
+	public bool SteeringRelativeToCar = true;
+
+	[Space]
+
+	[Tooltip("How much the camera is allowed to move horizontally")]
+	[Min(0)]
+	public float HorizontalBuffer;
+	[Tooltip("How much the camera is allowed to move vertically, min and max")]
+	public Vector2 VerticalBuffer;
+	[Tooltip("How much the camera is allowed to zoom, in and out")]
+	public Vector2 ZoomBuffer;
+
+	[Space]
+
+	public SteeringScript Car;
+	public Transform CameraPivot;
+	public Transform CameraLookat;
 
 	public Transform Parent;
 
-
 	Vector3 initPos;
+	float steeringBuffer = 0;
 
 	void Start() {
-		initPos = Quaternion.Inverse(CameraFocus.rotation) * (transform.position - CameraFocus.position);
+		initPos = Quaternion.Inverse(CameraPivot.rotation) * (transform.position - CameraPivot.position);
 
-		transform.parent = Parent;
+		if (Parent) {
+			transform.parent = Parent;
+		} else {
+			transform.parent = Car.transform.parent;
+		}
 	}
 
-	void FixedUpdate() {
+	void LateUpdate() {
 
-		// }
+		var carForward = CameraPivot.forward;
+		var velocityForward = Car.Velocity;
 
-		// void LateUpdate() {
-		var carForward = CameraFocus.forward;
-		var velocityForward = car.Velocity;
+		float steering = Car.GetSteering();
 
-		float steering = car.GetSteering();
+		Vector3 targetPos = CameraPivot.rotation * initPos;
 
-
-		Vector3 targetPos = CameraFocus.rotation * initPos;// + CameraFocus.position;
-
-		// transform.position = targetPos;
-		
 		// TODO: counteract/subtract car turn delta from camera turn follow
+		// TODO: reduce boost pad rotation change camera snapping
+		// IDEA: add snapping threshold for detecting jerky motion, use different logic in those cases
 
-		transform.position = Vector3.MoveTowards(transform.position, targetPos + CameraFocus.position, FollowSpeed * Time.deltaTime);
+		// TODO: separate follow speed for each axis
 
-		// Vector3 currentPos = transform.position;
-		Vector3 currentPos = Quaternion.Inverse(CameraFocus.rotation) * (transform.position - CameraFocus.position);
-		currentPos.x = Mathf.Clamp(currentPos.x, initPos.x - Buffer.x, initPos.x + Buffer.x);
-		currentPos.y = Mathf.Clamp(currentPos.y, initPos.y - Buffer.y, initPos.y + Buffer.y);
-		currentPos.z = Mathf.Clamp(currentPos.z, initPos.z - Buffer.z, initPos.z + Buffer.z);
-		// transform.position = currentPos;
-		transform.position = CameraFocus.rotation * currentPos + CameraFocus.position;
+		transform.position = Vector3.MoveTowards(transform.position, targetPos + CameraPivot.position, FollowSpeed * Time.deltaTime);
 
-		transform.LookAt(CameraFocus);
+		Vector3 currentPos = Quaternion.Inverse(CameraPivot.rotation) * (transform.position - CameraPivot.position);
+		currentPos.x = Mathf.Clamp(currentPos.x, initPos.x - HorizontalBuffer, initPos.x + HorizontalBuffer);
+		currentPos.y = Mathf.Clamp(currentPos.y, initPos.y + VerticalBuffer.x, initPos.y + VerticalBuffer.y);
+		currentPos.z = Mathf.Clamp(currentPos.z, initPos.z + ZoomBuffer.x, initPos.z + ZoomBuffer.y);
+		transform.position = CameraPivot.rotation * currentPos + CameraPivot.position;
+
+		transform.LookAt(CameraLookat);
+
+		// steeringBuffer = Mathf.MoveTowardsAngle(steeringBuffer, steering, SteeringFollowSpeed * Time.deltaTime);
+		steeringBuffer = Mathf.LerpAngle(steeringBuffer, steering, SteeringFollowSpeed * Mathf.Clamp(Mathf.Abs(steering), MinSteeringFollowMultiplier, 1f) * Time.deltaTime);
+		if (SteeringRelativeToCar) {
+			transform.rotation = Quaternion.AngleAxis(SteeringMax * steeringBuffer, CameraPivot.up) * transform.rotation;
+		} else {
+			transform.rotation = Quaternion.AngleAxis(SteeringMax * steeringBuffer, Vector3.up) * transform.rotation;
+		}
 
 		// transform.RotateAround(CameraFocus.position, CameraFocus.up, TurnSpeed*Time.deltaTime);
-
 
 
 	}
