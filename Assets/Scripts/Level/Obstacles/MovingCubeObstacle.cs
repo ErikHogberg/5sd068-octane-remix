93fdaf92299630aa033077ba707ab107ce66498e
Erik Högberg <erik.hogberg.9945@student.uu.se>
2020-05-25 20:25:56 +0200

    level progress. moving block obstacle progress

--- Assets/Scripts/Level/Obstacles/MovingCubeObstacle.cs
+++ Assets/Scripts/Level/Obstacles/MovingCubeObstacle.cs
@@ -5,9 +5,8 @@ using UnityEngine;
 [RequireComponent(typeof(Collider))]
 public class MovingCubeObstacle : MonoBehaviour {
 
-	public float AttackForce;
-	public float ResetForce;
-
+	public float AttackSpeed;
+	public float ResetSpeed;
 
 	[Space]
 
@@ -16,14 +15,24 @@ public class MovingCubeObstacle : MonoBehaviour {
 	public Transform OptionalTarget;
 
 	Vector3 currentTarget;
+	Vector3 initPos;
 	bool attack = false;
 
 	void Start() {
-
+		initPos = Cube.position;
 	}
 
 	void Update() {
-
+		if (attack) {
+			Vector3 pos = Vector3.MoveTowards(Cube.position, currentTarget, AttackSpeed);
+			Cube.MovePosition(pos);
+			if (Vector3.Distance(pos, Cube.position) == 0) {
+				attack = false;
+			}
+		} else {
+			Vector3 pos = Vector3.MoveTowards(Cube.position, initPos, ResetSpeed);
+			Cube.MovePosition(pos);
+		}
 	}
 
 	private void OnTriggerEnter(Collider other) {
