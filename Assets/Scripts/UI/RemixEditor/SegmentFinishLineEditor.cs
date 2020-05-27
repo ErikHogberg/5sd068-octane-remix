d0b94cb525ae55e657df40d7a8eabf8c3c4618c4
Erik Högberg <erik.hogberg.9945@student.uu.se>
2020-05-28 01:58:48 +0200

    changed colliders for all cars. remix editor lighting and occlusion culling baking.

--- Assets/Scripts/UI/RemixEditor/SegmentFinishLineEditor.cs
+++ Assets/Scripts/UI/RemixEditor/SegmentFinishLineEditor.cs
@@ -9,24 +9,36 @@ public class SegmentFinishLineEditor : SegmentEditorSuperClass {
 	public Toggle StartToggle;
 	public Toggle EndToggle;
 
-	public TMP_Text text;
+	public TMP_Text IsSelectedText;
+	public TMP_Text IsNotAllowedText;
 
 	protected override void ChildAwake() {
 
 	}
 
 	public override void UpdateUI() {
-		
+
 		StartToggle.isOn = currentSegment.isStart;
 		StartToggle.interactable = !currentSegment.isStart && currentSegment.IsStartable;
-		
+
 		EndToggle.isOn = currentSegment.isEnd;
 		EndToggle.interactable = !currentSegment.isEnd && currentSegment.IsEndable;
 
 		if (currentSegment.isStart || currentSegment.isEnd) {
-			text.enabled = true;
+			IsSelectedText.enabled = true;
 		} else {
-			text.enabled = false;
+			IsSelectedText.enabled = false;
+		}
+
+		IsNotAllowedText.enabled = true;
+		if (!currentSegment.IsStartable && !currentSegment.IsEndable) {
+			IsNotAllowedText.text ="This segment does not allow a start or finish line";
+		} else if (!currentSegment.IsStartable) {
+			IsNotAllowedText.text ="This segment does not allow a start line";
+		} else if (!currentSegment.IsEndable) {
+			IsNotAllowedText.text ="This segment does not allow a finish line";
+		} else {
+			IsNotAllowedText.enabled = false;
 		}
 
 	}
@@ -35,12 +47,14 @@ public class SegmentFinishLineEditor : SegmentEditorSuperClass {
 		if (!currentSegment.IsStartable)
 			return;
 		currentSegment.isStart = true;
+		UpdateUI();
 	}
 
 	public void EndOnToggle(bool value) {
 		if (!currentSegment.IsEndable)
 			return;
 		currentSegment.isEnd = true;
+		UpdateUI();
 	}
 
 }
