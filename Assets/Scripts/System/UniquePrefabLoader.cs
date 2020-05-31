f2102a3b5cffd138568977dc9d13bb604704d578
Erik Högberg <erik.hogberg.9945@student.uu.se>
2020-05-31 22:49:41 +0200

    fixed lvl loading issue

---
+++ Assets/Scripts/System/UniquePrefabLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UniquePrefabLoader : MonoBehaviour {
+	private static UniquePrefabLoader mainInstance;
+
+	public GameObject PrefabToLoad;
+
+	private void Awake() {
+		if (mainInstance) {
+			Destroy(gameObject);
+			return;
+		}
+
+		// SceneManager.LoadScene(SceneToAdd, LoadSceneMode.Additive);
+		var instance = Instantiate(PrefabToLoad);
+		mainInstance = this;
+		DontDestroyOnLoad(gameObject);
+		DontDestroyOnLoad(instance);
+	}
+
+	private void OnDestroy() {
+		if (mainInstance == this) {
+			mainInstance = null;
+		}
+	}
+}
