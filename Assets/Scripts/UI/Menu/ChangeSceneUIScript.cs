10517bb55a4e4c328597b6fc6ce83173264403c2
Erik Högberg <erik.hogberg.9945@student.uu.se>
2020-06-03 19:22:21 +0200

    attempt at loading bar on scene transition

---
+++ Assets/Scripts/UI/Menu/ChangeSceneUIScript.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneUIScript : MonoBehaviour {
+
+	public string CurrentScene;
+
+	private void Awake() {
+		if (!SceneManager.GetSceneByName(CurrentScene).IsValid()) {
+			string oldCurrentScene = CurrentScene;
+			CurrentScene = SceneManager.GetActiveScene().name;
+			Debug.LogWarning("Current scene (" + oldCurrentScene + ") not found, using \"" + CurrentScene + "\" instead");
+		}
+		// if (DebugOutput) {
+		// 	Debug.Log("Current scene: " + currentScene);
+		// }
+	}
+
+	public void StartScene(string sceneName) {
+		Time.timeScale = 1f;
+		SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+	}
+
+	// AsyncOperation loadSceneProgress = null;
+	// AsyncOperation unloadSceneProgress = null;
+
+	bool loading = false;
+
+	public void SwapCurrentScene(string SceneToSwapTo) {
+		if (loading)
+			return;
+
+		loading = true;
+
+		if (!SceneManager.GetSceneByName(CurrentScene).IsValid()) {
+			Debug.LogError("Chosen current scene does not exist: " + CurrentScene);
+			Quit();
+			return;
+		}
+
+		// var unloadProgress =
+		// var unloadSceneProgress = 
+		 SceneManager.UnloadSceneAsync(CurrentScene);
+		// StartCoroutine(UnloadScene(CurrentScene));
+
+		// unloadSceneProgress.allowSceneActivation = false;
+		// var loadProgress = 
+		// var loadSceneProgress = 
+		SceneManager.LoadSceneAsync(SceneToSwapTo, LoadSceneMode.Additive);
+		// loadSceneProgress.allowSceneActivation = false;
+		// StartCoroutine(LoadScene(SceneToSwapTo));
+
+
+	}
+
+	// private void Update() {
+	// 	if (loadSceneProgress.isDone) {
+	// 		loadSceneProgress.allowSceneActivation = true;
+	// 		return;
+	// 	}
+	// 	SceneLoadBarScript.SetProgress(loadSceneProgress.progress, unloadSceneProgress.progress);
+	// }
+
+	IEnumerator LoadScene(string sceneToLoad) {
+		yield return null;
+
+		//Begin to load the Scene you specify
+		AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneToLoad, LoadSceneMode.Additive);
+		//Don't let the Scene activate until you allow it to
+		asyncOperation.allowSceneActivation = false;
+		// Debug.Log("Pro :" + asyncOperation.progress);
+		// SceneLoadBarScript.SetProgress(asyncOperation.progress, 0.5f);
+
+		//When the load is still in progress, output the Text and progress bar
+		while (!asyncOperation.isDone) {
+			//Output the current progress
+			// m_Text.text = "Loading progress: " + (asyncOperation.progress * 100) + "%";
+			SceneLoadBarScript.SetProgress(asyncOperation.progress, 0.5f);
+
+			// Check if the load has finished
+			if (asyncOperation.progress >= 0.9f) {
+				//Change the Text to show the Scene is ready
+				// m_Text.text = "Press the space bar to continue";
+				//Wait to you press the space key to activate the Scene
+				// if (Input.GetKeyDown(KeyCode.Space))
+				//Activate the Scene
+				asyncOperation.allowSceneActivation = true;
+			}
+
+			yield return null;
+		}
+	}
+
+	IEnumerator UnloadScene(string sceneToUnload) {
+		yield return null;
+
+		AsyncOperation asyncOperation = SceneManager.UnloadSceneAsync(sceneToUnload);
+		asyncOperation.allowSceneActivation = false;
+		while (!asyncOperation.isDone) {
+			SceneLoadBarScript.SetProgress(asyncOperation.progress, 0.5f);
+
+			if (asyncOperation.progress >= 0.9f) {
+				asyncOperation.allowSceneActivation = true;
+			}
+
+			yield return null;
+		}
+	}
+
+	public void Quit() {
+#if UNITY_STANDALONE
+		Application.Quit();
+#endif
+
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+#endif
+	}
+
+}
---
+++ Assets/Scripts/UI/Menu/SceneLoadBarScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BarUIScript))]
+public class SceneLoadBarScript : MonoBehaviour {
+
+	private static SceneLoadBarScript mainInstance;
+
+	public GameObject ObjectToHide;
+
+	// private BarUIScript bar;
+	public BarUIScript loadBar;
+	public BarUIScript unloadBar;
+
+	private void Awake() {
+		mainInstance = this;
+		// bar = GetComponent<BarUIScript>();
+		Hide();
+	}
+
+	private void OnDestroy() {
+		if (mainInstance == this) {
+			mainInstance = null;
+		}
+	}
+
+	public static void SetVisible(bool visible) {
+		if (!mainInstance)
+			return;
+
+		if (mainInstance.ObjectToHide) {
+			mainInstance.ObjectToHide.SetActive(visible);
+		} else {
+			mainInstance.transform.parent.gameObject.SetActive(visible);
+		}
+
+	}
+
+	public static void Show() {
+		SetVisible(true);
+	}
+
+	public static void Hide() {
+		SetVisible(false);
+	}
+
+	public static void SetProgress(float loadProgress, float unloadProgress) {
+		if (!mainInstance)
+			return;
+
+		Show();
+		mainInstance.loadBar.SetBarPercentage(loadProgress);
+		mainInstance.unloadBar.SetBarPercentage(unloadProgress);
+	}
+
+
+}
