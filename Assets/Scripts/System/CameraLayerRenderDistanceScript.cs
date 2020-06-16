7d68c600364c298bf2ea122f9e9d510d5c942817
Erik Högberg <erik.hogberg.9945@student.uu.se>
2020-06-17 00:23:25 +0200

    wheel collider friction is now set in steering script profile. added untested velocity cap on rock collision. set up custom object render distance to work with car camera on obstacles

--- Assets/Scripts/System/CameraLayerRenderDistanceScript.cs
+++ Assets/Scripts/System/CameraLayerRenderDistanceScript.cs
@@ -13,19 +13,26 @@ public class CameraLayerRenderDistanceScript : MonoBehaviour {
 
 	public LayerRenderDistancePair[] LayerSettings;
 
-	void Awake() {
+	public bool PrintDebug = false;
+
+	void Start() {
 		Camera camera = GetComponent<Camera>();
 
 		float[] distances = new float[32];
 
 		foreach (var item in LayerSettings) {
 			int layerIndex = LayerMask.NameToLayer(item.LayerName);
-			if (layerIndex < 0)
+			if (layerIndex < 0) {
+				if (PrintDebug)
+					Debug.Log("[Render distance] Layer " + item.LayerName + " not found ");
+
 				continue;
+			}
 
 			distances[layerIndex] = item.RenderDistance;
+			if (PrintDebug)
+				Debug.Log("Successfully set render distance of layer " + item.LayerName + " to " + item.RenderDistance);
 		}
-
 
 		camera.layerCullDistances = distances;
 	}
