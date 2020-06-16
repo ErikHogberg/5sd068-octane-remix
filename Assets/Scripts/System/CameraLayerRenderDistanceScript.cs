08bfccceeb462eaee02e7c53ad8075b327dc8882
Erik Högberg <erik.hogberg.9945@student.uu.se>
2020-06-16 18:31:17 +0200

    added untested script for setting object render distance

---
+++ Assets/Scripts/System/CameraLayerRenderDistanceScript.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLayerRenderDistanceScript : MonoBehaviour {
+
+	[Serializable]
+	public struct LayerRenderDistancePair {
+		public string LayerName;
+		public float RenderDistance;
+	}
+
+	public LayerRenderDistancePair[] LayerSettings;
+
+	void Awake() {
+		Camera camera = GetComponent<Camera>();
+
+		float[] distances = new float[32];
+
+		foreach (var item in LayerSettings) {
+			int layerIndex = LayerMask.NameToLayer(item.LayerName);
+			if (layerIndex < 0)
+				continue;
+
+			distances[layerIndex] = item.RenderDistance;
+		}
+
+
+		camera.layerCullDistances = distances;
+	}
+
+}
