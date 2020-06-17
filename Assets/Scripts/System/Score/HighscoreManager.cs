d87e9d299617acaae3bde855b490c00dc5965942
Erik Högberg <erik.hogberg.9945@student.uu.se>
2020-06-17 17:22:14 +0200

    minor highscore list progress

---
+++ Assets/Scripts/System/Score/HighscoreManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class HighscoreManager {
+
+	public class HighscoreEntry {
+		public int Score;
+		public int Time;
+		public CharacterSelected Character;
+		public string RemixID;
+	}
+
+	public static List<HighscoreEntry> Entries;
+
+	public static void LoadHighscoreList(){
+		
+	}
+
+}
--- Assets/Scripts/UI/RemixEditor/HighscoreListUIScript.cs
+++ Assets/Scripts/UI/RemixEditor/HighscoreListUIScript.cs
@@ -4,12 +4,7 @@ using UnityEngine;
 
 public class HighscoreListUIScript : MonoBehaviour {
 
-	public class HighscoreEntry {
-		public int Score;
-		public int Time;
-		public CharacterSelected Character;
-		public string RemixID;
-	}
+	
 
 	void Start() {
 
