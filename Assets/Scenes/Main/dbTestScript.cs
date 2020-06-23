3cc53378374747e39743680c7cc1cfb589ae25b4
Erik Högberg <erik.hogberg.9945@student.uu.se>
2020-06-23 21:32:18 +0200

    highscore db progress wip

---
+++ Assets/Scenes/Main/dbTestScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dbTestScript : MonoBehaviour {
+
+	// HighscoreManager.HighscoreList list;
+
+	void Start() {
+		HighscoreList list = new HighscoreList();
+		// list = new HighscoreManager.HighscoreList();
+		list.Start();
+		list = null;
+	}
+
+}
