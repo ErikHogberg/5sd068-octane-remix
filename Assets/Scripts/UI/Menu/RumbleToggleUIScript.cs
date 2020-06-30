db8c6a0fe90fa7d2693cf9a7ec84e67b268d5a23
Erik Högberg <erik.hogberg.9945@student.uu.se>
2020-06-30 20:25:00 +0200

    added arrow signs to track. added rumble toggle to settings

---
+++ Assets/Scripts/UI/Menu/RumbleToggleUIScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Toggle))]
+public class RumbleToggleUIScript : MonoBehaviour {
+
+	Toggle toggle;
+	
+	void Start() {
+		toggle.isOn = SteeringScript.EnableRumble;
+
+	}
+
+	public void Toggle(bool value){
+		SteeringScript.EnableRumble = value;
+	}
+
+}
