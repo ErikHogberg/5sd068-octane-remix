fd6dd892b1448620bc777eb8d35d1da0aa296a08
Erik Högberg <erik.hogberg.9945@student.uu.se>
2020-07-07 21:22:29 +0200

    bday meme scenes

---
+++ Assets/bday/timescaleScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timescaleScript : MonoBehaviour
+{
+    
+    public float Timescale = 1f;
+    
+    void Start()
+    {
+        Time.timeScale = Timescale;
+    }
+
+}
