a7c295c6412bb1869f23d1220de412ee34ebf89b
Erik Högberg <erik.hogberg.9945@student.uu.se>
2020-08-29 01:55:13 +0200

    tiling segment bending tool wip

---
+++ Assets/Scripts/Level/TilingSegmentBendingScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class TilingSegmentBendingScript : MonoBehaviour {
+
+	public Transform Target;
+	public Transform TargetMagnitude;
+	public Transform StartMagnitude;
+
+	public GameObject Segment;
+
+	public float MinLength;
+	public float MaxLength;
+
+	void Start() {
+
+	}
+
+	void Update() {
+		if (!Target || !TargetMagnitude || !StartMagnitude) {
+			return;
+		}
+
+		// TODO: cache updating
+
+		// TODO: get curve
+		// TODO: measure length of curve
+		// TODO: decide number of segments
+		// TODO: place, bend and snap segments
+	}
+}
