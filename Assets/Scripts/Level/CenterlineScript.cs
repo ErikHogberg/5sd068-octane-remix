c48843b1d35a65ec9119e547e658619e286528f4
Erik Högberg <erik.hogberg.9945@student.uu.se>
2020-12-30 21:39:02 +0100

    added skeleton for centerline script with editor

---
+++ Assets/Scripts/Level/CenterlineScript.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterlineScript : MonoBehaviour {
+
+	public List<Vector3> ControlPoints;
+
+	// TODO: get closest point to curve relative to given point
+
+}
